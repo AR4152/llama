4f3f24a8b1f403cb2aeadead9d401e29a8635e3a
Arjun Raj <87035926+AR4152@users.noreply.github.com>
2024-12-03 20:52:33 +0000

    improve readibility

--- query.py
+++ query.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+# MLHub toolkit for llama - query
+# ml query llama
+
+# -----------------------------------------------------------------------
+# Command line argument and options
+# -----------------------------------------------------------------------
+
+def cli():
+    """
+    Simple `hello world` string output.
+    """
+    print("Hello world!")
+
+    
+if __name__ == "__main__":
+    cli()
